ef161e0ca0270ecaa7032d9230184472355b5a8a
DHARANIKA-M <dharanikamr@gmail.com>
2026-04-02 23:23:15 +0530

    Update sales_app.py

--- sales_app.py
+++ sales_app.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import pandas as pd
 
+st.title("📊 Sales Dashboard App")
+st.subheader("Filter and visualize product sales by category")
+
+data = {
+    "Product": ["Laptop", "Shirt", "Mobile", "Jeans", "Tablet"],
+    "Category": ["Electronics", "Clothing", "Electronics", "Clothing", "Electronics"],
+    "Sales": [50000, 15000, 30000, 20000, 25000]
+}
+
+df = pd.DataFrame(data)
+
+st.sidebar.header("Filter Options")
+category = st.sidebar.selectbox("Select Category", df["Category"].unique())
+
+filtered_df = df[df["Category"] == category]
+
+st.write(f"### Showing data for: {category}")
+st.dataframe(filtered_df)
+
+st.write("### Sales Trend")
+st.line_chart(filtered_df.set_index("Product")["Sales"])
